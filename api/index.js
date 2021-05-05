fe1a8e1c7faef776f603fc92bc1dbf409c047686
kashifulhaque <haque.kashiful7@gmail.com>
2021-05-05 13:15:35 +0530

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const app = require("express")();
+const ytdl = require("ytdl-core");
+
+app.get("/api", async (req, res) => {
+  const { url } = await req.query;
+  const { formats } = await ytdl.getInfo(url);
+
+  const stats = formats.map((format) => {
+    const { qualityLabel, url, hasVideo, hasAudio } = format;
+    const obj = {
+      qualityLabel: qualityLabel,
+      url: url,
+      hasVideo: hasVideo,
+      hasAudio: hasAudio,
+    };
+
+    return obj;
+  });
+
+  res.json(stats);
+});
+
+module.exports = app;
